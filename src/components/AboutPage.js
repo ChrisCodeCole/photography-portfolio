faa71744d0976eee10dfe1a1217cf5b64723a793
Craig Coleman <CColeSV07@Gmail.com>
2020-01-19 06:20:25 -0500

    About Page Updates

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,19 +1,65 @@
 import React from "react";
 import "../styles/AboutPage.css";
 
+function parallax() {
+    let s = document.getElementById("floater"); //get ID
+    let yPos = 0 - window.pageYOffset / 5; // Y Position =   Nothing -   PageYOffeset
+    s.style.top = 50 + yPos + "%"; //style.top = 50 + (0 - window.pageYoffset / 5) + %
+}
+
+console.log(window.pageYoffset);
+
+window.addEventListener("scroll", function() {
+    parallax();
+});
+
+let dynamicNumber;
+
+// let f = function() {
+//     let eventHandler = function(event) {
+//         let divElement=document.getElementsByTagName('div')[4];
+//         divElement.textContent = 'X=' + window.pageXoffset + 'Y=' + window.pageYoffset;
+//     };
+
+//     window.addEventListener("scroll", eventHandler, false);
+
+// document.addEventListener("DOMContentLoaded", f, false);
+
 export default class AboutPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            scrollY: 0
+        };
+    }
+
+    componentDidMount() {
+        window.addEventListener("scroll", this.handleScroll);
+    }
+
+    handleScroll = () => {
+        this.setState({ scrollY: window.scrollY });
+    };
+
     render() {
         return (
             <div className="AboutPage-mainContainer">
                 <div className="Middle-Container">
                     <div className="Left-Side">
-                        <p className="left">Testing</p>
+                        <p className="left-text">Testing</p>
                     </div>
                     <div className="Right-Side">
-                        <p className="right">Test2</p>
+                        <p className="right-text" id="floater">
+                            Test2
+                            {/* {dynamicNumber} */}
+                            <div>{this.state.scrollY} </div>
+                        </p>
                     </div>
                 </div>
             </div>
         );
+    }
+    componentDidUnmount() {
+        window.removeEventListener("scroll", this.handleScroll);
     }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from "react";
-import AboutPage from "./AboutPage";
 import { FaBars } from "react-icons/fa";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "../styles/Header.css";

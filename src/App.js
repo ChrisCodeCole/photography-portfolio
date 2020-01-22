27ab6638df04cfde48d61a41ea5a6cba3423e6f8
Craig Coleman <CColeSV07@Gmail.com>
2020-01-21 23:44:01 -0500

    New Parrallax First Try

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import AboutPage from "./components/AboutPage";
 import MainPage from "./components/MainPage";
+import { ParallaxProvider } from "react-scroll-parallax";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/" exact component={MainPage} />
-          <Route path="/about" component={AboutPage} />
-        </Switch>
-      </div>
-    </Router>
+    <ParallaxProvider>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path="/" exact component={MainPage} />
+            <Route path="/about" component={AboutPage} />
+          </Switch>
+        </div>
+      </Router>
+    </ParallaxProvider>
   );
 }
 
--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,16 +1,17 @@
 import React from "react";
+import { ParallaxProvider } from "react-scroll-parallax";
 import "../styles/AboutPage.css";
 
 function parallax() {
-    let s = document.getElementById("floater"); //get ID
-    let yPos = 0 - window.pageYOffset / 5; // Y Position =   Nothing -   PageYOffeset
-    s.style.top = 50 + yPos + "%"; //style.top = 50 + (0 - window.pageYoffset / 5) + %
+  let s = document.getElementById("floater"); //get ID
+  let yPos = 0 - window.pageYOffset / 5; // Y Position =   Nothing -   PageYOffeset
+  s.style.top = 50 + yPos + "%"; //style.top = 50 + (0 - window.pageYoffset / 5) + %
 }
 
 console.log(window.pageYoffset);
 
 window.addEventListener("scroll", function() {
-    parallax();
+  parallax();
 });
 
 let dynamicNumber;
@@ -25,41 +26,47 @@ let dynamicNumber;
 
 // document.addEventListener("DOMContentLoaded", f, false);
 
+// const ParallaxImage = () => (
+//     <Parallax className="custom-class" y={[-20, 20]} tagOuter="figure">
+//         <Image src="/image.jpg" />
+//     </Parallax>
+// );
+
 export default class AboutPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            scrollY: 0
-        };
-    }
-
-    componentDidMount() {
-        window.addEventListener("scroll", this.handleScroll);
-    }
-
-    handleScroll = () => {
-        this.setState({ scrollY: window.scrollY });
+  constructor(props) {
+    super(props);
+    this.state = {
+      scrollY: 0
     };
+  }
 
-    render() {
-        return (
-            <div className="AboutPage-mainContainer">
-                <div className="Middle-Container">
-                    <div className="Left-Side">
-                        <p className="left-text">Testing</p>
-                    </div>
-                    <div className="Right-Side">
-                        <p className="right-text" id="floater">
-                            Test2
-                            {/* {dynamicNumber} */}
-                            <div>{this.state.scrollY} </div>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-    componentDidUnmount() {
-        window.removeEventListener("scroll", this.handleScroll);
-    }
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = () => {
+    this.setState({ scrollY: window.scrollY });
+  };
+
+  render() {
+    return (
+      <div className="AboutPage-mainContainer">
+        <div className="Middle-Container">
+          <div className="Left-Side">
+            <p className="left-text">Testing</p>
+          </div>
+          <div className="Right-Side">
+            <p className="right-text" id="floater">
+              Test2
+              {/* {dynamicNumber} */}
+              <div>{this.state.scrollY} </div>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  componentDidUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
 }

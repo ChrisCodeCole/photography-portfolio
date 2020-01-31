1b85a97d7e972ec58ef9cc85ddbb09c6b6e6ef41
Craig Coleman <CColeSV07@Gmail.com>
2020-01-30 23:51:15 -0500

    About Page In-Progress

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import parallax from "../assets/parallax.jpg";
+import parallax from "../assets/parallax.jpg";
 import Header from "./Header";
 import Footer from "./Footer";
 import "../styles/AboutPage.css";
@@ -29,13 +29,13 @@ export default class AboutPage extends React.Component {
       // down
       this.setState(prevState => ({
         rightScrollY: prevState.rightScrollY + 10,
-        leftScrollY: prevState.leftScrollY + 30
+        leftScrollY: prevState.leftScrollY - 20
       }));
     } else if (event.wheelDelta > 0) {
       //up
       this.setState(prevState => ({
         rightScrollY: prevState.rightScrollY - 10,
-        leftScrollY: prevState.leftScrollY - 30
+        leftScrollY: prevState.leftScrollY + 20
       }));
     }
   };
@@ -51,7 +51,34 @@ export default class AboutPage extends React.Component {
               ref={this.leftDivRef}
               style={{ transform: `translateY(${this.state.leftScrollY}%)` }}
             >
-              <p>TEST LEFT</p>
+              <div className="left-text-container">
+                <p className="left-text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                  rhoncus eu tellus sit amet faucibus. In hac habitasse platea
+                  dictumst. Fusce pretium orci vitae leo consequat vulputate.
+                  Aliquam tincidunt feugiat erat, in mattis ante convallis ac.
+                  Orci varius natoque penatibus et magnis dis parturient montes,
+                  nascetur ridiculus mus.
+                </p>
+                <p className="left-text">
+                  Praesent pellentesque iaculis arcu, non consequat nunc
+                  ultrices sit amet. Aenean feugiat odio odio, id vulputate orci
+                  eleifend et. Donec sed luctus odio. Aliquam sit amet consequat
+                  velit. Aenean interdum, metus quis dignissim sodales, neque
+                  purus ullamcorper elit, non maximus leo nulla et lectus. Duis
+                  luctus ligula erat, mattis viverra lacus eleifend vitae. Sed
+                  sit amet eros ut eros pellentesque elementum. Donec sit amet
+                  suscipit nibh.
+                </p>
+                <p className="left-text">
+                  Aenean ac fringilla ex. Quisque a suscipit neque. Donec et
+                  orci risus. Vivamus at augue et mi maximus sagittis. Vivamus
+                  accumsan, mauris consequat luctus suscipit, leo nisi fermentum
+                  odio, sit amet maximus justo sapien et turpis. Pellentesque
+                  felis lorem, porttitor et luctus et, cursus semper turpis.
+                  Cras eleifend nulla sit amet efficitur sodales
+                </p>
+              </div>
             </div>
           </div>
           {/* transform: `translateX(${this.state.translateValue * -1}px)` */}
@@ -62,6 +89,7 @@ export default class AboutPage extends React.Component {
               style={{ transform: `translateY(${this.state.rightScrollY}%)` }}
             >
               <p>TEST RIGHT</p>
+              {/* <img src="../assets/parallax.jpg"></img> */}
             </div>
           </div>
         </div>
